8e99b84895961d4041cdb2a3ec566db45f1b2620
Syed Ibrahim Ahmed <34831534+hamdani92@users.noreply.github.com>
2019-03-03 01:21:48 +0100

    Add files via upload

---
+++ Week2/homework/maartjes-work.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const monday = [{
+    name: 'Write a summary HTML/CSS',
+    duration: 180
+  },
+  {
+    name: 'Some web development',
+    duration: 120
+  },
+  {
+    name: 'Fix homework for class10',
+    duration: 20
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200
+  }
+];
+
+const tuesday = [{
+    name: 'Keep writing summary',
+    duration: 240
+  },
+  {
+    name: 'Some more web development',
+    duration: 180
+  },
+  {
+    name: 'Staring out the window',
+    duration: 10
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200
+  },
+  {
+    name: 'Look at application assignments new students',
+    duration: 40
+  }
+];
+
+const maartjesTasks = monday.concat(tuesday);
+// eslint-disable-next-line no-unused-vars
+const maartjesHourlyRate = 20;
+
+// Map the tasks to durations in hours.
+const tasksInHour = maartjesTasks.map(tasks => tasks.duration / 60);
+
+// Filter out everything that took less than two hours
+const longTasks = tasksInHour.filter(task => task >= 2);
+
+// Multiply the each duration by a per-hour rate for billing (use €20/hour) and sum it all up
+const profits = longTasks.map(task => task * 20);
+
+let totalProfit = 0;
+profits.forEach(function (profit) {
+  totalProfit += profit;
+});
+
+// Output a formatted Euro amount, rounded to Euro cents, e.g: €11.34
+const roundedTotalProfit = totalProfit.toFixed(2);
+
+// add code to convert `earnings` to a string rounded to two decimals (euro cents)
+console.log(`Maartje has earned €${roundedTotalProfit}`);
